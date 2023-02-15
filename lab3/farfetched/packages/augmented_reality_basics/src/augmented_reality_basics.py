9293d27c5b65d94d25b7f21e08525a54aac40802
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-02-15 15:51:01 -0700

    draw_segment

--- lab3/farfetched/packages/augmented_reality_basics/src/augmented_reality_basics.py
+++ lab3/farfetched/packages/augmented_reality_basics/src/augmented_reality_basics.py
@@ -1,6 +1,19 @@
+import cv2
+import numpy as np
+
+
 class Augmenter:
     def __init__(self) -> None:
-        return
+        self.defined_colors = {
+            'red': ['rgb', [1, 0, 0]],
+            'green': ['rgb', [0, 1, 0]],
+            'blue': ['rgb', [0, 0, 1]],
+            'yellow': ['rgb', [1, 1, 0]],
+            'magenta': ['rgb', [1, 0, 1]],
+            'cyan': ['rgb', [0, 1, 1]],
+            'white': ['rgb', [1, 1, 1]],
+            'black': ['rgb', [0, 0, 0]]
+        }
 
     def process_image(self, image):
         """Undistorts raw images.
@@ -18,3 +31,31 @@ class Augmenter:
         """Plots the segments from the map files onto the image.
         """
         return
+
+    def draw_segment(
+            self,
+            image: np.ndarray,
+            pt_a: np.ndarray,
+            pt_b: np.ndarray,
+            color: str):
+        """Draw segment on image
+
+        based off https://docs.duckietown.org/daffy/
+        duckietown-classical-robotics/out/
+        cra_basic_augmented_reality_exercise.html
+
+        Args:
+            image (np.ndarray): image to draw on
+            pt_a (np.ndarray): start point of segment
+            pt_b (np.ndarray): end point of segment
+            color (str): color of segment
+        """
+        _, [r, g, b] = self.defined_colors[color]
+        cv2.line(
+            image,
+            pt_a,
+            pt_b,
+            (b * 255, g * 255, r * 255),
+            5
+        )
+        return image
