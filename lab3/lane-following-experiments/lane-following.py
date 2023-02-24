b26fe96dc099328110bad11ba85edc2e8a78f1f9
Akemi Izuko <akemi@noway.moe>
2023-02-24 15:11:14 -0700

    Split masks into separate channels

--- lab3/lane-following-experiments/lane-following.py
+++ lab3/lane-following-experiments/lane-following.py
@@ -4,26 +4,32 @@ import numpy as np
 cap = cv2.VideoCapture('apriltag.mov')
 
 
-def get_hsv_mask(hsv: np.ndarray, lower: np.ndarray, upper: np.ndarray):
-    mask = cv2.inRange(hsv, lower, upper)
-    return mask
+def rgb2bgr(r, g, b):
+    return [b, g, r]
 
+def mask_range_rgb(image, lower: list, upper: list, fill: list):
+    return mask_range(image, rgb2bgr(*lower), rgb2bgr(*upper), rgb2bgr(*fill))
+
+def mask_range(image, lower: list, upper: list, fill: list):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, np.array(lower), np.array(upper))
+    image[mask > 0] = fill
+    return image
 
 def lane_geometry(image: np.ndarray):
     """
     https://www.tutorialspoint.com/detection-of-a-specific-color-blue-here-using-opencv-with-python
     https://docs.opencv.org/3.4/d4/d73/tutorial_py_contours_begin.html
     """
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        # White detection
+    image = mask_range_rgb(image, [160, 0, 0],   [255, 61, 255], [255]*3)
+        # Red detection
+    image = mask_range_rgb(image, [130, 100, 0], [250, 250, 20], [255, 0, 0])
+        # Yellow detection
+    image = mask_range_rgb(image, [100, 40, 0], [240, 255, 80], [255, 255, 0])
+        # Black out
+    image = mask_range_rgb(image, [0]*3, [200]*3, [0]*3)
 
-    gray_mask = get_hsv_mask(hsv, np.array([0, 18, 0]), np.array([255, 180, 118]))
-    white_mask = get_hsv_mask(hsv, np.array([0, 0, 160]), np.array([255, 61, 255]))
-    yellow_mask = get_hsv_mask(hsv, np.array([10, 30, 100]), np.array([40, 160, 200]))
-    red_mask = get_hsv_mask(hsv, np.array([0, 100, 100]), np.array([20, 255, 255]))
-    image[gray_mask > 0] = [128, 128, 128]
-    image[white_mask > 0] = [255, 255, 255]
-    image[yellow_mask > 0] = [0, 255, 255]
-    image[red_mask > 0] = [0, 0, 255]
     return image
 
 def draw_lane_geometry(image, geometry):
@@ -32,11 +38,22 @@ def draw_lane_geometry(image, geometry):
 while cap.isOpened():
     ret, image = cap.read()
     image = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
-    image = image[118:-10, 30:-220]  # Crop to camera only
-    geometry = lane_geometry(image)
-    image = draw_lane_geometry(image, geometry)
-    cv2.imshow('image', image)
+    #image = image[118:-10, 30:-220]  # Crop to camera only
+    image = image[318:-10, 30:-220]  # Crop to lower camera
+
+    white_channel = mask_range_rgb(image.copy(), [160, 0, 0],   [255, 61, 255], [255]*3)
+    white_channel = mask_range_rgb(white_channel, [0]*3, [254, 254, 254], [0]*3)
+    red_channel = mask_range_rgb(image.copy(), [130, 100, 0], [255, 255, 20], [255, 0, 0])
+    red_channel = mask_range_rgb(red_channel, [0]*3, [254, 255, 255], [0]*3)
+    yellow_channel = mask_range_rgb(image.copy(), [100, 40, 0], [240, 255, 80], [255, 255, 0])
+    yellow_channel = mask_range_rgb(yellow_channel, [0]*3, [254, 254, 255], [0]*3)
+
+    cv2.imshow('white', white_channel)
+    cv2.imshow('red', red_channel)
+    cv2.imshow('yellow', yellow_channel)
+
     key = cv2.waitKey(1)
+
     if key == ord('q'):
         break
 
--- lab3/lane-following-experiments/startenv.sh
+++ lab3/lane-following-experiments/startenv.sh
@@ -3,7 +3,7 @@ if ! [[ -d bin ]]; then
   python3 -mvenv .
 fi
 
-if ! [[ -f apriltags.mov ]]; then
+if ! [[ -f apriltag.mov ]]; then
   echo "WARNING: no apriltags video found!"
 fi
 
