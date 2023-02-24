a09e9c8bcff829317dc421ab60a353ff72c46938
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-02-23 20:16:47 -0700

    Apriltag node

---
+++ lab3/farfetched/packages/apriltag/src/apriltag_node.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+import math
+import os
+import sys
+from typing import List
+
+import cv2
+import numpy as np
+import rospkg
+import rospy
+import yaml
+from cv_bridge import CvBridge, CvBridgeError
+from dt_apriltags import Detection, Detector
+from duckietown.dtros import DTROS, NodeType
+from duckietown_msgs.msg import LEDPattern
+from image_geometry import PinholeCameraModel
+from sensor_msgs.msg import CameraInfo, CompressedImage
+from std_msgs.msg import ColorRGBA
+from tag import Tag, TagType, TAG_ID_TO_TAG
+
+"""
+
+This is a template that can be used as a starting point for the CRA1 exercise.
+You need to project the model file in the 'models' directory on an AprilTag.
+To help you with that, we have provided you with the Renderer class that render the obj file.
+
+"""
+
+
+class AprilTagNode(DTROS):
+    def __init__(self, node_name):
+        super(AprilTagNode, self).__init__(node_name=node_name,
+                                           node_type=NodeType.GENERIC)
+
+        self.hostname = rospy.get_param("~veh")
+        self.bridge = CvBridge()
+
+        self.detector = Detector(
+            searchpath=['apriltags'],
+            families='tag36h11',
+            nthreads=1,
+            quad_decimate=1.0,
+            quad_sigma=0.0,
+            refine_edges=1,
+            decode_sharpening=0.25,
+            debug=0
+        )
+
+        self.camera_model = PinholeCameraModel()
+
+        # Standard subscribers and publishers
+        self.pub = rospy.Publisher(
+            '~compressed', CompressedImage, queue_size=1
+        )
+
+        self.led_pattern_pub = rospy.Publisher(
+            f'/{self.hostname}/led_emitter_node/led_pattern',
+            LEDPattern,
+            queue_size=10,
+        )
+        self.raw_image = None
+
+        self.compressed_sub = rospy.Subscriber(
+            f'/{self.hostname}/camera_node/image/compressed',
+            CompressedImage,
+            self.cb_compressed,
+        )
+
+        self.camera_info_sub = rospy.Subscriber(
+            f"/{self.hostname}/camera_node/camera_info",
+            CameraInfo,
+            self.cb_camera_info
+        )
+
+    def process_image(self, raw):
+        """Undistorts raw images.
+        """
+        rectified = np.zeros_like(raw)
+        self.camera_model.rectifyImage(raw, rectified)
+        return rectified
+
+    def cb_camera_info(self, message):
+        """Callback for the camera_node/camera_info topic."""
+        self.camera_model.fromCameraInfo(message)
+
+    def cb_compressed(self, compressed):
+        self.raw_image = self.bridge.compressed_imgmsg_to_cv2(compressed)
+
+    def detect(self, image):
+        return self.detector.detect(
+            image,
+            estimate_tag_pose=False,
+            camera_params=None,
+            tag_size=None
+        )
+
+    def render_tag(self, image: np.ndarray, detection: Detection):
+        for i in range(4):
+            tag = TAG_ID_TO_TAG.get(
+                detection.tag_id, Tag(detection.tag_id, None)
+            )
+            corner_a = detection.corners[i]
+            corner_b = detection.corners[(i + 1) % 4]
+            cv2.line(
+                image,
+                corner_a.astype(np.int64),
+                corner_b.astype(np.int64),
+                tag.color,
+                2
+            )
+            text = str(detection.tag_id)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            center = detection.center.astype(np.int64)
+            font_scale = 1
+            font_thickness = 2
+            text_size = cv2.getTextSize(text, font, font_scale, font_thickness)[0]
+            center[0] -= text_size[0] // 2
+            center[1] += text_size[1] // 2
+            cv2.putText(
+                image,
+                text,
+                center,
+                font,
+                font_scale,
+                tag.color[::-1],
+                font_thickness
+            )
+
+    def get_led_color(self, detections: List[Detection]):
+        white = (255, 255, 255)
+        priority_tag = None
+        for detection in detections:
+            tag = TAG_ID_TO_TAG.get(detection.tag_id, Tag(detection.tag_id, None))
+            if tag is None:
+                rospy.logwarn(f"Unknown tag id: {tag.tag_id}")
+                continue
+            if tag.type is TagType.StopSign:
+                priority_tag = tag
+                break
+            if tag.type is TagType.TIntersection:
+                priority_tag = tag
+            elif tag.type is TagType.UofA and priority_tag is None:
+                priority_tag = tag
+
+        return priority_tag.color if priority_tag else white
+
+    def create_led_message(self, colors: 'List[float]') -> 'LEDPattern':
+        """ Creates an led message with the colors set to values from a tuple
+
+        Args:
+            colors (list[float]): RGB values from 0 to 255
+        """
+        led_message = LEDPattern()
+        for _ in range(5):
+            rgba = ColorRGBA()
+            rgba.r = colors[0] / 255
+            rgba.g = colors[1] / 255
+            rgba.b = colors[2] / 255
+            rgba.a = 1.0
+            led_message.rgb_vals.append(rgba)
+        return led_message
+
+    def rectify(self, image):
+        """Undistorts raw images.
+        """
+        rectified = np.zeros_like(image)
+        self.camera_model.rectifyImage(image, rectified)
+        return rectified
+
+    def run(self, rate=30):
+        rate = rospy.Rate(rate)
+
+        while not rospy.is_shutdown():
+            if self.raw_image is None:
+                rate.sleep()
+                continue
+            image = self.raw_image.copy()
+            image = self.rectify(image)
+            grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            tags = self.detect(grayscale)
+            for tag in tags:
+                self.render_tag(image, tag)
+            color = self.get_led_color(tags)
+            message = self.bridge.cv2_to_compressed_imgmsg(
+                image, dst_format="jpeg"
+            )
+            self.pub.publish(message)
+            led_message = self.create_led_message(color)
+            self.led_pattern_pub.publish(led_message)
+            rate.sleep()
+
+    def onShutdown(self):
+        super(AprilTagNode, self).onShutdown()
+
+
+if __name__ == '__main__':
+    camera_node = AprilTagNode(node_name='apriltag_node')
+    camera_node.run()
