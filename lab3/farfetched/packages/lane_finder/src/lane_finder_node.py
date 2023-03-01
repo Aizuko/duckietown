34a51249fe38b476d3629b5c1d05585b04cd35a3
Akemi Izuko <akemi@noway.moe>
2023-03-01 15:33:23 -0700

    Interm progress

--- lab3/farfetched/packages/lane_finder/src/lane_finder_node.py
+++ lab3/farfetched/packages/lane_finder/src/lane_finder_node.py
@@ -4,6 +4,7 @@ import time
 from pathlib import Path
 
 import cv2
+import cv2 as cv
 import numpy as np
 import rospy
 import yaml
@@ -20,29 +21,50 @@ from duckietown_msgs.srv import (
 from sensor_msgs.msg import CompressedImage, CameraInfo
 from std_msgs.msg import ColorRGBA
 
+RED = (0,0,255)
+BLUE = (255,0,0)
+GREEN = (0,255,0)
+TEAL = (255,255,0)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
 
-def rgb2bgr(r, g, b):
-    return [b, g, r]
 
-def mask_range_rgb(image, lower: list, upper: list, fill: list):
-    return mask_range(image, rgb2bgr(*lower), rgb2bgr(*upper), rgb2bgr(*fill))
+def min_dist_in_set(points: list) -> list:
+    """ Returns the two points with the minimal euclidean norm """
+    if len(points) < 2:
+        return None
 
-def mask_range(image, lower: list, upper: list, fill: list):
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, np.array(lower), np.array(upper))
-    image[mask > 0] = fill
-    return image
+    min_dist = float("inf")
+    best = None
+
+    for i in range(len(points)):
+        for j in range(i+1, len(points)):
+            a = np.array(points[i])
+            b = np.array(points[j])
+
+            if cv.norm(a-b) < min_dist:
+                min_dist = cv.norm(a-b)
+                best = [a, b]
+
+    return best
 
 
 class LaneFinderNode(DTROS):
     def __init__(self, node_name):
         super(LaneFinderNode, self).__init__(
             node_name=node_name,
-            node_type=NodeType.DRIVER)
+            node_type=NodeType.DRIVER,
+        )
 
         self.hostname = rospy.get_param("~veh")
         self.bridge = CvBridge()
         self.raw_image = None
+        self.processed_image = None
+        self.horizontal_target_err = None
+        self.is_processed = False
+
+        self.low_hsv = (21, 15, 134)
+        self.high_hsv = (66, 255, 255)
 
         # Setup publisher path ====
         self.pub = rospy.Publisher(
@@ -51,116 +73,133 @@ class LaneFinderNode(DTROS):
             queue_size=2,
         )
 
-        self.pub_white = rospy.Publisher(
-            f"/{self.hostname}/lane_finder_node/debug/white/compressed",
-            CompressedImage,
-            queue_size=1,
-        )
-
         self.pub_yellow = rospy.Publisher(
             f"/{self.hostname}/lane_finder_node/debug/yellow/compressed",
             CompressedImage,
             queue_size=1,
         )
 
+        self.pub_raw = rospy.Publisher(
+            f"/{self.hostname}/lane_finder_node/debug/raw/compressed",
+            CompressedImage,
+            queue_size=1,
+        )
+
         self.img_sub = rospy.Subscriber(
-            f"/{self.hostname}/homography_publisher/image/compressed",
+            #f"/{self.hostname}/homography_publisher/image/compressed",
+            f"/{self.hostname}/camera_node/image/compressed",
             CompressedImage,
             self.callback_image
         )
 
-        self.white_x = None
-        self.yellow_x = None
-        return
-
     def callback_image(self, message):
-        """Callback for the /camera_node/image/compressed topic."""
+        self.is_processed = False
         self.raw_image = self.bridge.compressed_imgmsg_to_cv2(
             message, desired_encoding='passthrough'
         )
+        self.is_processed = False
 
-    def channel_masking(self, image: np.ndarray):
-        image = image[len(image)//2:, :]  # Only use bottom half
+    def process(self):
+        image = self.raw_image
+        image = image[image.shape[0]//3:4*image.shape[0]//5, :]
+        image = cv.blur(image, (5,5))
 
-        white_channel = mask_range_rgb(image.copy(), [160, 0, 0],   [255, 61, 255], [255]*3)
-        red_channel = mask_range_rgb(image.copy(), [130, 100, 0], [255, 255, 20], [255]*3)
-        yellow_channel = mask_range_rgb(image.copy(), [100, 40, 0], [240, 255, 80], [255]*3)
+        x, y, _ = image.shape
 
-        white_channel = mask_range_rgb(white_channel, [0]*3, [254]*3, [0]*3)
-        red_channel = mask_range_rgb(red_channel, [0]*3, [254]*3, [0]*3)
-        yellow_channel = mask_range_rgb(yellow_channel, [0]*3, [254]*3, [0]*3)
+        self.pub_raw.publish(
+            self.bridge.cv2_to_compressed_imgmsg(image, dst_format="jpeg")
+        )
 
-        white_grey = cv2.cvtColor(white_channel, cv2.COLOR_BGR2GRAY)
-        white_conts, _ = cv2.findContours(white_grey, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, self.low_hsv, self.high_hsv)
 
-        if len(white_conts) > 0:
-            c = max(white_conts, key=cv2.contourArea)
-            M = cv2.moments(c)
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            self.white_x = cx
-            self.white_y = cy
-            cv2.line(white_channel, (cx,0),(cx,720), (0,255,0),1)
-            cv2.line(white_channel, (0,cy),(1280,cy),(0,255,0),1)
-            cv2.drawContours(white_channel, white_conts, -1, (0,255,0), 1)
-        else:
-            self.white_x = None
-            self.white_y = None
+        image[mask == 0] = BLACK
+        image[mask != 0] = WHITE
 
-        yellow_grey = cv2.cvtColor(yellow_channel, cv2.COLOR_BGR2GRAY)
-        yellow_conts, _ = cv2.findContours(yellow_grey, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        image_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        conts, _ = cv2.findContours(image_grey, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        w = self.bridge.cv2_to_compressed_imgmsg(white_channel, dst_format="jpeg")
-        self.pub_white.publish(w)
+        if len(conts) > 0:
+            conts_sort = sorted(conts, key=lambda x: -cv2.contourArea(x))
 
-        if len(yellow_conts) > 0:
-            c = max(yellow_conts, key=cv2.contourArea)
-            M = cv2.moments(c)
-            cx = int(M['m10']/(M['m00'] or 1))
-            cy = int(M['m01']/(M['m00'] or 1))
-            self.yellow_x = cx
-            self.yellow_y = cy
+            for i in range(4):
+                if i >= len(conts_sort):
+                    return
 
-            cv2.line(yellow_channel, (cx,0),(cx,720), (0,255,0),1)
-            cv2.line(yellow_channel, (0,cy),(1280,cy),(0,255,0),1)
-            cv2.drawContours(yellow_channel, yellow_conts, -1, (0,255,0), 1)
-        else:
-            self.yellow_x = None
-            self.yellow_y = None
+                c = conts_sort[i]
 
-        y = self.bridge.cv2_to_compressed_imgmsg(yellow_channel, dst_format="jpeg")
-        self.pub_yellow.publish(y)
-        return
+                M = cv2.moments(c)
+                cx = int(M['m10']/(M['m00'] or 1))
+                cy = int(M['m01']/(M['m00'] or 1))
+
+                contour_center = np.array([cx, cy])
+
+                #rospy.loginfo(f"{0.3*y} < {cy} < {0.7*y} :: {y}")
+                #if not (0.3*y < cy < 0.7*y):
+                #    continue
+
+                cv2.circle(image, (cx, cy), 8,  RED, -1)
+
+                rect = cv2.minAreaRect(c)
+                box = np.intp(cv2.boxPoints(rect))
+                cv2.drawContours(image, [box], 0, RED, 2)
+
+                ext_left = tuple(c[c[:, :, 0].argmin()][0])
+                ext_right = tuple(c[c[:, :, 0].argmax()][0])
+                ext_top = tuple(c[c[:, :, 1].argmin()][0])
+                ext_bot = tuple(c[c[:, :, 1].argmax()][0])
+
+                cv2.drawContours(image, [c], 0, GREEN, 1)
+                cv2.circle(image, ext_left, 8,  RED, -1)
+                cv2.circle(image, ext_right, 8, RED, -1)
+                cv2.circle(image, ext_top, 8,   RED, -1)
+                cv2.circle(image, ext_bot, 8,   RED, -1)
+
+                points = [ ext_left, ext_right, ext_top, ext_bot ]
+
+                #if (m := min_dist_in_set(points)) is not None:
+                #    if cv.norm(m[0], m[1]) < 5:
+                #        continue
+                #    elif m[0][0] < m[1][0]:
+                #        a, b = m[1], m[0]
+                #    else:
+                #        a, b = m
+
+                #unit = (a-b) / cv.norm(a-b)
+                #scaled = np.intp(unit * cy) + np.array([cx,cy])
+                self.target = (cx + cy, cy)
+                rospy.loginfo(f"{self.target}, {x//2}, {x}, {y}, {cx}, {cy}")
+                self.horizontal_target_err = self.target[0] - x;
+
+                cv2.line(image, (cx, cy), self.target, TEAL, 3)
+                #cv2.line(image, a, b, TEAL, 3)
+                break
+
+        self.processed_image = image
+        self.is_processed = True
 
     def run(self):
-        rate = rospy.Rate(5)
+        rate = rospy.Rate(3)
 
         while not rospy.is_shutdown():
-            if self.raw_image is not None:
-                self.channel_masking(self.raw_image)
-            msg = FarfetchedPose()
+            if self.raw_image is not None and not self.is_processed:
+                self.process()
 
-            if self.yellow_x is not None and self.yellow_y is not None:
-                msg.yellow_x = self.yellow_x
-                msg.yellow_y = self.yellow_y
-                msg.is_yellow_detected = True
-            else:
-                msg.is_yellow_detected = False
+            if self.processed_image is not None:
+                msg = self.bridge.cv2_to_compressed_imgmsg(
+                    self.processed_image,
+                    dst_format="jpeg",
+                )
+                self.pub_yellow.publish(msg)
 
-            if self.white_x is not None and self.white_y is not None:
-                msg.white_x = self.white_x
-                msg.white_y = self.white_y
-                msg.is_white_detected = True
-            else:
-                msg.is_white_detected = False
-
-            self.pub.publish(msg)
-            rospy.loginfo("Publishing message")
+                pose = FarfetchedPose()
+                pose.horizontal_target_err = self.horizontal_target_err
+                pose.is_yellow_detected = True
+                #rospy.loginfo(pose)
 
             rate.sleep()
 
-
 if __name__ == "__main__":
-    ar_node = LaneFinderNode(node_name="lane_finder_node")
-    ar_node.run()
+    node = LaneFinderNode(node_name="lane_finder_node")
+    node.run()
     rospy.spin()
