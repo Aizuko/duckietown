7c8a3e30b19a3e7242b962a6d02971b7a29a8827
Akemi Izuko <akemi@noway.moe>
2023-03-09 17:23:18 -0700

    Remove turbojpeg

--- lab4/ucb_player2/packages/lane_follower/src/lane_follow_node.py
+++ lab4/ucb_player2/packages/lane_follower/src/lane_follow_node.py
@@ -1,45 +1,49 @@
 #!/usr/bin/env python3
 
 import rospy
+import cv2
 
 from duckietown.dtros import DTROS, NodeType
 from sensor_msgs.msg import CameraInfo, CompressedImage
 from std_msgs.msg import Float32
-from turbojpeg import TurboJPEG
-import cv2
+from cv_bridge import CvBridge
 import numpy as np
 from duckietown_msgs.msg import WheelsCmdStamped, Twist2DStamped
 
 ROAD_MASK = [(20, 60, 0), (50, 255, 255)]
+STOP_MASK = [(0, 70, 150), (20, 255, 255)]
 DEBUG = True
 ENGLISH = False
 
 class LaneFollowNode(DTROS):
-
     def __init__(self, node_name):
         super(LaneFollowNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
         self.node_name = node_name
         self.veh = rospy.get_param("~veh")
 
         # Publishers & Subscribers
-        self.pub = rospy.Publisher("/" + self.veh + "/output/image/mask/compressed",
+        self.pub = rospy.Publisher(f"/{self.veh}/output/image/mask/compressed",
                                    CompressedImage,
                                    queue_size=1)
-        self.sub = rospy.Subscriber("/" + self.veh + "/camera_node/image/compressed",
+        self.pub_red = rospy.Publisher(
+                                   f"/{self.veh}/output/image/red/compressed",
+                                   CompressedImage,
+                                   queue_size=1)
+        self.sub = rospy.Subscriber(f"/{self.veh}/camera_node/image/compressed",
                                     CompressedImage,
-                                    self.callback,
+                                    self.ajoin_callback,
                                     queue_size=1,
                                     buff_size="20MB")
-        self.vel_pub = rospy.Publisher("/" + self.veh + "/car_cmd_switch_node/cmd",
+        self.vel_pub = rospy.Publisher(f"/{self.veh}/car_cmd_switch_node/cmd",
                                        Twist2DStamped,
                                        queue_size=1)
 
-        self.jpeg = TurboJPEG()
+        self.bridge = CvBridge()
 
         self.loginfo("Initialized")
 
         # PID Variables
-        self.proportional = None
+        self.error = None
         if ENGLISH:
             self.offset = -220
         else:
@@ -47,69 +51,99 @@ class LaneFollowNode(DTROS):
         self.velocity = 0.4
         self.twist = Twist2DStamped(v=self.velocity, omega=0)
 
-        self.P = 0.049
-        self.D = -0.004
         self.last_error = 0
         self.last_time = rospy.get_time()
+
+        # Stopline variables
+        self.is_stopped = True
+        self.stop_time = None
+
+        # Constants
+        self.P = 0.049
+        self.D = -0.004
+        self.stop_duration = 1
 
         # Shutdown hook
         rospy.on_shutdown(self.hook)
 
-    def callback(self, msg):
-        img = self.jpeg.decode(msg.data)
+    def ajoin_callback(self, msg):
+        print("Called ajoined")
+        self.lane_callback(msg)
+        self.stop_callback(msg)
+
+    def lane_callback(self, msg):
+        img = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
         crop = img[300:-1, :, :]
         crop_width = crop.shape[1]
         hsv = cv2.cvtColor(crop, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, ROAD_MASK[0], ROAD_MASK[1])
         crop = cv2.bitwise_and(crop, crop, mask=mask)
-        contours, hierarchy = cv2.findContours(mask,
-                                               cv2.RETR_EXTERNAL,
-                                               cv2.CHAIN_APPROX_NONE)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL,
+                                       cv2.CHAIN_APPROX_NONE)
 
         # Search for lane in front
-        max_area = 20
-        max_idx = -1
-        for i in range(len(contours)):
-            area = cv2.contourArea(contours[i])
-            if area > max_area:
-                max_idx = i
-                max_area = area
+        areas = np.array([cv2.contourArea(a) for a in contours])
 
-        if max_idx != -1:
+        if len(areas) == 0 or np.max(areas) < 20:
+            self.error = None
+        else:
+            max_idx = np.argmax(areas)
+
             M = cv2.moments(contours[max_idx])
             try:
                 cx = int(M['m10'] / M['m00'])
                 cy = int(M['m01'] / M['m00'])
-                self.proportional = cx - int(crop_width / 2) + self.offset
+                self.error = cx - int(crop_width / 2) + self.offset
                 if DEBUG:
                     cv2.drawContours(crop, contours, max_idx, (0, 255, 0), 3)
                     cv2.circle(crop, (cx, cy), 7, (0, 0, 255), -1)
             except:
                 pass
-        else:
-            self.proportional = None
 
         if DEBUG:
-            rect_img_msg = CompressedImage(format="jpeg", data=self.jpeg.encode(crop))
+            rect_img_msg = self.bridge.cv2_to_compressed_imgmsg(crop)
             self.pub.publish(rect_img_msg)
 
+    def stop_callback(self, msg):
+        img = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
+        crop = img[300:-1, :, :]
+        crop_width = crop.shape[1]
+        hsv = cv2.cvtColor(crop, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, STOP_MASK[0], STOP_MASK[1])
+        crop = cv2.bitwise_and(crop, crop, mask=mask)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL,
+                                       cv2.CHAIN_APPROX_NONE)
+
+        areas = np.array([cv2.contourArea(a) for a in contours])
+
+        print(np.flip(np.sort(areas))[:4])
+
+        if DEBUG:
+            rect_img_msg = self.bridge.cv2_to_compressed_imgmsg(crop)
+            self.pub_red.publish(rect_img_msg)
+
     def drive(self):
-        if self.proportional is None:
+        if self.error is None:
+            self.twist.omega = 0
+        elif self.is_stopped:
+            self.twist.v = 0
             self.twist.omega = 0
         else:
             # P Term
-            P = -self.proportional * self.P
+            P = -self.error * self.P
 
             # D Term
-            d_error = (self.proportional - self.last_error) / (rospy.get_time() - self.last_time)
-            self.last_error = self.proportional
+            d_error = (self.error - self.last_error) \
+                / (rospy.get_time() - self.last_time)
+            self.last_error = self.error
             self.last_time = rospy.get_time()
             D = d_error * self.D
 
             self.twist.v = self.velocity
             self.twist.omega = P + D
-            if DEBUG:
-                self.loginfo(f"{self.proportional} {P} {D} {self.twist.omega} {self.twist.v}")
+            #if DEBUG:
+            #    self.loginfo(
+            #        f"{self.error} {P} {D} {self.twist.omega} {self.twist.v}")
 
         self.vel_pub.publish(self.twist)
 
