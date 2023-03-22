5e4d81e7e417887ea031cb5cf4ba2fcf5bedd1b6
Akemi Izuko <akemi@noway.moe>
2023-03-22 14:54:21 -0600

    Setup service server

--- lab5/mallardvision/packages/mallard_eye/src/mallard_eye_node.py
+++ lab5/mallardvision/packages/mallard_eye/src/mallard_eye_node.py
@@ -11,68 +11,32 @@ from geometry_msgs.msg import Quaternion, Transform, TransformStamped, Vector3
 from image_geometry import PinholeCameraModel
 from sensor_msgs.msg import CameraInfo, CompressedImage
 from std_msgs.msg import ColorRGBA, Header
-from tag import TAG_ID_TO_TAG, Tag, TagType
-from tf import transformations as tr
-from tf2_ros import Buffer, ConnectivityException, TransformBroadcaster, TransformListener
 
-"""
-
-This is a template that can be used as a starting point for the CRA1 exercise.
-You need to project the model file in the 'models' directory on an Number.
-To help you with that, we have provided you with the Renderer class that render the obj file.
-
-"""
+from led_controls.srv import MallardEyedentify, MallardEyedentifyResponse
 
 
 class MallardEyeNode(DTROS):
     def __init__(self, node_name):
-        super(MallardEyeNode, self).__init__(node_name=node_name,
-                                           node_type=NodeType.GENERIC)
+        super(MallardEyeNode, self).__init__(
+            node_name=node_name, node_type=NodeType.GENERIC
+        )
 
         self.hostname = rospy.get_param("~veh")
         self.bridge = CvBridge()
 
-    def process_image(self, raw):
-        """Undistorts raw images.
-        """
-        rectified = np.zeros_like(raw)
-        self.camera_model.rectifyImage(raw, rectified)
-        return rectified
+        self.serv = rospy.Service(
+            "mallard_eyedentification", MallardEyedentify, self.identify
+        )
+        rospy.loginfo("Started led_control_service")
+        return
 
-    def cb_camera_info(self, message):
-        """Callback for the camera_node/camera_info topic."""
-        self.camera_model.fromCameraInfo(message)
-
-    def cb_compressed(self, compressed):
-        self.raw_image = self.bridge.compressed_imgmsg_to_cv2(compressed)
-
-    def run(self, rate=3):
-        rate = rospy.Rate(rate)
-
-        while not rospy.is_shutdown():
-            if self.raw_image is None:
-                rate.sleep()
-                continue
-            image = self.raw_image.copy()
-            image = self.rectify(image)
-            grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            detections = self.detect(grayscale)
-            for detection in detections:
-                self.render_tag(image, detection)
-            self.broadcast_transforms(detections)
-            color = self.get_led_color(detections)
-            message = self.bridge.cv2_to_compressed_imgmsg(
-                image, dst_format="jpeg"
-            )
-            self.img_pub.publish(message)
-            led_message = self.create_led_message(color)
-            self.led_pattern_pub.publish(led_message)
-            rate.sleep()
+    def identify(self, srv: MallardEyedentify) -> MallardEyedentifyResponse:
+        pass
 
     def onShutdown(self):
         super(MallardEyeNode, self).onShutdown()
 
 
-if __name__ == '__main__':
-    camera_node = MallardEyeNode(node_name='mallard_eye_node')
+if __name__ == "__main__":
+    camera_node = MallardEyeNode(node_name="mallard_eye_node")
     camera_node.run(1)
