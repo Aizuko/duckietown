825407296a4f1eca9581df5639c70ea3091bf2f2
Akemi Izuko <akemi@noway.moe>
2023-03-23 16:24:24 -0600

    Fix more mallard eye stuff

--- lab5/mallardvision/packages/mallard_eye/src/mallard_eye_node.py
+++ lab5/mallardvision/packages/mallard_eye/src/mallard_eye_node.py
@@ -5,7 +5,7 @@ import rospy
 from cv_bridge import CvBridge
 from duckietown.dtros import DTROS, NodeType
 from mallard_eye.srv import MallardEyedentify, MallardEyedentifyResponse
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, CameraInfo
 from nn import Net
 from preprocess import normalize_image, preprocess_image, warp_image
 from image_geometry import PinholeCameraModel
@@ -23,6 +23,7 @@ class MallardEyeNode(DTROS):
         self.net = Net(weights_path="/weights.npy")
         self.annotated_image = None
         self.compressed = None
+        self.is_camera_init = False
 
         self.pub = rospy.Publisher(
             f"/{self.hostname}/mallard_eye_node/image/compressed",
@@ -41,10 +42,23 @@ class MallardEyeNode(DTROS):
             queue_size=1,
         )
 
+        self.camera_info_sub = rospy.Subscriber(
+            f"/{self.hostname}/camera_node/camera_info",
+            CameraInfo,
+            self.cb_camera_info,
+            queue_size=1,
+        )
+
         rospy.loginfo("Started mallard eye")
 
     def cb_compressed(self, compressed):
         self.compressed = compressed
+
+    def cb_camera_info(self, message):
+        """Callback for the camera_node/camera_info topic."""
+        self.camera_model.fromCameraInfo(message)
+        self.camera_info_sub.unregister()
+        self.is_camera_init = True
 
     def set_compressed(
         self,
@@ -55,7 +69,7 @@ class MallardEyeNode(DTROS):
     ):
         image_copy = image.copy()
         image_copy[:64, :64] = np.repeat(
-            cv.resize(image_warped, (32, 32))[:, :, np.newaxis], 3, axis=2
+            cv.resize(image_warped, (64, 64))[:, :, np.newaxis], 3, axis=2
         )
         image_copy.drawContours([corners], -1, (0, 255, 255), 2)
 
@@ -71,32 +85,21 @@ class MallardEyeNode(DTROS):
         self.annotated_image = self.bridge.cv2_to_compressed_imgmsg(image_copy)
 
     def identify(self, _) -> MallardEyedentifyResponse:
-        rospy.loginfo(f"Starting a detection 1 with camera type {type(self.camera_model)}")
         if self.compressed is not None:
-            rospy.loginfo("Starting a detection 2")
             raw_image = self.bridge.compressed_imgmsg_to_cv2(self.compressed)
-            rospy.loginfo("Starting a detection 3")
             rectified_image = np.zeros_like(raw_image)
-            rospy.loginfo("Starting a detection 4")
-            rospy.loginfo(f"Types: {type(raw_image)}, {type(rectified_image)}")
             self.camera_model.rectifyImage(raw_image, rectified_image)
-            rospy.loginfo("Starting a detection 5")
 
             image_warped, corners = warp_image(rectified_image)
-            rospy.loginfo("Starting a detection 6")
             if image_warped is None:
-                rospy.loginfo("Starting a detection -1")
                 return -1
 
-            rospy.loginfo("Starting a detection 7")
             image_warped = preprocess_image(image_warped)
-            rospy.loginfo("Starting a detection 8")
             x = normalize_image(image_warped)
-            rospy.loginfo("Starting a detection 9")
 
             digit = self.net.predict(x)
             rospy.loginfo("Starting a detection 10")
-            self.set_compressed(image, image_warped, corners, digit)
+            self.set_compressed(rectified_image, image_warped, corners, digit)
             rospy.loginfo("Starting a detection 11")
 
             rospy.loginfo(f"Prediction: {digit}")
