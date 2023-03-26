41d09f31f78eb6bce02e862cd8bc54c348fdf11f
Akemi Izuko <akemi@noway.moe>
2023-03-26 03:43:12 -0600

    Back to identification

--- lab5/mallardvision/packages/apriltag/src/apriltag_node.py
+++ lab5/mallardvision/packages/apriltag/src/apriltag_node.py
@@ -14,7 +14,12 @@ from sensor_msgs.msg import CameraInfo, CompressedImage
 from std_msgs.msg import ColorRGBA, Header, Float64
 from tag import TAG_ID_TO_TAG, Tag, TagType
 from tf import transformations as tr
-from tf2_ros import Buffer, ConnectivityException, TransformBroadcaster, TransformListener
+from tf2_ros import (
+    Buffer,
+    ConnectivityException,
+    TransformBroadcaster,
+    TransformListener,
+)
 
 """
 
@@ -27,8 +32,9 @@ To help you with that, we have provided you with the Renderer class that render 
 
 class AprilTagNode(DTROS):
     def __init__(self, node_name):
-        super(AprilTagNode, self).__init__(node_name=node_name,
-                                           node_type=NodeType.GENERIC)
+        super(AprilTagNode, self).__init__(
+            node_name=node_name, node_type=NodeType.GENERIC
+        )
 
         self.hostname = rospy.get_param("~veh")
         self.bridge = CvBridge()
@@ -42,14 +48,14 @@ class AprilTagNode(DTROS):
         }
 
         self.detector = Detector(
-            searchpath=['apriltags'],
-            families='tag36h11',
+            searchpath=["apriltags"],
+            families="tag36h11",
             nthreads=1,
             quad_decimate=1.0,
             quad_sigma=0.0,
             refine_edges=1,
             decode_sharpening=0.25,
-            debug=0
+            debug=0,
         )
 
         self.camera_model = PinholeCameraModel()
@@ -57,31 +63,29 @@ class AprilTagNode(DTROS):
 
         # Standard subscribers and publishers
         self.img_pub = rospy.Publisher(
-            '~compressed',
-            CompressedImage,
-            queue_size=1
+            "~compressed", CompressedImage, queue_size=1
         )
 
         self.pub_teleport = rospy.Publisher(
             f"/{self.hostname}/deadreckoning_node/teleport",
             Transform,
-            queue_size=1
+            queue_size=1,
         )
 
         self.pub_ap_distance = rospy.Publisher(
             f"/{self.hostname}/deadreckoning_node/ap_distance",
             Float64,
-            queue_size=1
+            queue_size=1,
         )
 
         self.pub_ap_position = rospy.Publisher(
-            f"/{self.hostname}/ap_node/ap_position,
+            f"/{self.hostname}/ap_node/ap_position",
             Vector3,
             queue_size=1,
         )
 
         self.compressed_sub = rospy.Subscriber(
-            f'/{self.hostname}/camera_node/image/compressed',
+            f"/{self.hostname}/camera_node/image/compressed",
             CompressedImage,
             self.cb_compressed,
             queue_size=1,
@@ -99,8 +103,7 @@ class AprilTagNode(DTROS):
         self.tf_listener = TransformListener(self.tf_buffer)
 
     def process_image(self, raw):
-        """Undistorts raw images.
-        """
+        """Undistorts raw images."""
         rectified = np.zeros_like(raw)
         self.camera_model.rectifyImage(raw, rectified)
         return rectified
@@ -120,9 +123,9 @@ class AprilTagNode(DTROS):
                 self.camera_model.fx(),
                 self.camera_model.fy(),
                 self.camera_model.cx(),
-                self.camera_model.cy()
+                self.camera_model.cy(),
             ],
-            tag_size=0.05
+            tag_size=0.05,
         )
 
     def render_tag(self, image: np.ndarray, detection: Detection):
@@ -138,30 +141,24 @@ class AprilTagNode(DTROS):
                 corner_a.astype(np.int64),
                 corner_b.astype(np.int64),
                 bgr,
-                2
+                2,
             )
             text = str(detection.tag_id)
             font = cv2.FONT_HERSHEY_SIMPLEX
             center = detection.center.astype(np.int64)
             font_scale = 1
             font_thickness = 2
-            text_size = cv2.getTextSize(
-                text, font, font_scale, font_thickness)[0]
+            text_size = cv2.getTextSize(text, font, font_scale, font_thickness)[
+                0
+            ]
             center[0] -= text_size[0] // 2
             center[1] += text_size[1] // 2
             cv2.putText(
-                image,
-                text,
-                center,
-                font,
-                font_scale,
-                bgr,
-                font_thickness
+                image, text, center, font, font_scale, bgr, font_thickness
             )
 
     def rectify(self, image):
-        """Undistorts raw images.
-        """
+        """Undistorts raw images."""
         rectified = np.zeros_like(image)
         self.camera_model.rectifyImage(image, rectified)
         return rectified
@@ -179,14 +176,13 @@ class AprilTagNode(DTROS):
 
             q = tr.quaternion_from_matrix(T_at_camera)
             transform = Transform(
-                translation=Vector3(*translation),
-                rotation=Quaternion(*q)
+                translation=Vector3(*translation), rotation=Quaternion(*q)
             )
             tag_id = detection.tag_id
             transform_stamped = TransformStamped(
                 header=Header(
                     stamp=rospy.Time.now(),
-                    frame_id=f"{self.hostname}/camera_optical_frame"
+                    frame_id=f"{self.hostname}/camera_optical_frame",
                 ),
                 child_frame_id=f"at_{tag_id}",
                 transform=transform,
@@ -203,7 +199,7 @@ class AprilTagNode(DTROS):
                     f"at_{closest_tag_id}",
                     "odometry",
                     rospy.Time(0),
-                    rospy.Duration(1.0)
+                    rospy.Duration(1.0),
                 ).transform
             except ConnectivityException as e:
                 rospy.logwarn_throttle(1.0, str(e))
@@ -212,47 +208,54 @@ class AprilTagNode(DTROS):
                 rospy.logwarn_throttle(1.0, "Another exception fired")
                 return
             T_odometry_at = tr.compose_matrix(
-                translate=([
-                    transform_odometry_at.translation.x,
-                    transform_odometry_at.translation.y,
-                    transform_odometry_at.translation.z
-                ]),
-                angles=tr.euler_from_quaternion([
-                    transform_odometry_at.rotation.x,
-                    transform_odometry_at.rotation.y,
-                    transform_odometry_at.rotation.z,
-                    transform_odometry_at.rotation.w
-                ])
+                translate=(
+                    [
+                        transform_odometry_at.translation.x,
+                        transform_odometry_at.translation.y,
+                        transform_odometry_at.translation.z,
+                    ]
+                ),
+                angles=tr.euler_from_quaternion(
+                    [
+                        transform_odometry_at.rotation.x,
+                        transform_odometry_at.rotation.y,
+                        transform_odometry_at.rotation.z,
+                        transform_odometry_at.rotation.w,
+                    ]
+                ),
             )
             try:
                 transform_at_static_world = self.tf_buffer.lookup_transform(
                     "world",
                     f"at_{closest_tag_id}_static",
                     rospy.Time(0),
-                    rospy.Duration(1.0)
+                    rospy.Duration(1.0),
                 ).transform
             except Exception as e:
                 rospy.logwarn_throttle(1.0, str(e))
                 return
             T_at_static_world = tr.compose_matrix(
-                translate=([
-                    transform_at_static_world.translation.x,
-                    transform_at_static_world.translation.y,
-                    transform_at_static_world.translation.z
-                ]),
-                angles=tr.euler_from_quaternion([
-                    transform_at_static_world.rotation.x,
-                    transform_at_static_world.rotation.y,
-                    transform_at_static_world.rotation.z,
-                    transform_at_static_world.rotation.w
-                ])
+                translate=(
+                    [
+                        transform_at_static_world.translation.x,
+                        transform_at_static_world.translation.y,
+                        transform_at_static_world.translation.z,
+                    ]
+                ),
+                angles=tr.euler_from_quaternion(
+                    [
+                        transform_at_static_world.rotation.x,
+                        transform_at_static_world.rotation.y,
+                        transform_at_static_world.rotation.z,
+                        transform_at_static_world.rotation.w,
+                    ]
+                ),
             )
             T_odometry_world = T_at_static_world @ T_odometry_at
             translation = tr.translation_from_matrix(T_odometry_world)
             q = tr.quaternion_from_matrix(T_odometry_world)
             transform_odometry_world = Transform(
-                translation=Vector3(*translation),
-                rotation=Quaternion(*q)
+                translation=Vector3(*translation), rotation=Quaternion(*q)
             )
 
             self.pub_ap_distance.publish(min_distance)
@@ -283,6 +286,6 @@ class AprilTagNode(DTROS):
         super(AprilTagNode, self).onShutdown()
 
 
-if __name__ == '__main__':
-    camera_node = AprilTagNode(node_name='apriltag_node')
+if __name__ == "__main__":
+    camera_node = AprilTagNode(node_name="apriltag_node")
     camera_node.run()
